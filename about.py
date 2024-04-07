30c31f4fb712c70f57d4c3fa2a8b9156388ff0ba
ARUN JOHN  M S <157452839+Runa8147@users.noreply.github.com>
2024-04-07 10:07:52 +0530

    Add files via upload

---
+++ about.py
@@ -0,0 +1,56 @@
+import streamlit as st
+
+
+st.title("About Nutrify")
+st.subheader("Nutrify: Your AI-Powered Partner for a Healthier You")
+
+  # Introduction
+st.write("""
+  Nutrify is an innovative chatbot designed to make achieving your nutritional goals easier and more accessible. 
+  Powered by Gemini, a cutting-edge large language model from Google AI, Nutrify provides personalized guidance 
+  and information to help you navigate the world of nutrition.
+  """)
+
+  # Our Mission
+st.subheader("Our Mission")
+st.write("""
+  At Nutrify, we believe everyone deserves access to reliable and easy-to-understand information about nutrition. 
+  Our mission is to empower individuals to make informed dietary choices that promote overall health and wellbeing.
+  """)
+
+  # How We Work
+st.subheader("How We Work")
+st.write("""
+  Nutrify utilizes the power of AI to analyze your questions and provide insightful responses. 
+  Whether you're curious about specific foods, seeking meal planning advice, or simply want to understand healthy 
+  eating habits better, Nutrify is here to assist you.
+  """)
+
+  # Key Features
+st.subheader("Key Features")
+feature_list = [
+      "Conversational Interface: Ask Nutrify questions in a natural, chat-like way.",
+      "Personalized Guidance: Receive tailored advice based on your individual needs and preferences.",
+      "Evidence-Based Information: Nutrify's responses are grounded in scientific research and reliable sources.",
+      "Accessible and User-Friendly: Nutrify is designed to be easy to use for everyone, regardless of technical expertise."
+  ]
+  
+st.subheader("The Team Behind Nutrify (Optional)")
+st.write("""
+  Nutrify is brought to you by a passionate team of developers, nutritionists, and AI enthusiasts dedicated to creating 
+  innovative tools that empower a healthier future.  **[ Briefly introduce key members or link to a Team page ]**
+  """)
+
+  # Looking Ahead
+st.subheader("Looking Ahead")
+st.write("""
+  We are constantly working on improving Nutrify and expanding its capabilities. We are committed to providing users 
+  with the most up-to-date information and innovative features to support their nutritional journey.
+  """)
+
+  # Contact Us
+st.subheader("Contact Us:")
+st.write("""
+  We value your feedback! If you have any questions or suggestions, please don't hesitate to contact us at [runanothingisimpossible@gmail.com].""")
+
+
