961d6e0acc34599300aa1515548a73ddfaf9a785
ARUN JOHN  M S <157452839+Runa8147@users.noreply.github.com>
2024-04-08 18:55:49 +0530

    Add files via upload

---
+++ homex.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Nutrify ai")
+st.subheader("Nutrify: Your AI-Powered Partner for a Healthier You")
+st.image("logo.png",width=100)
+st.write("""
+  Nutrify is an innovative chatbot designed to make achieving your nutritional goals easier and more accessible. 
+  Powered by Gemini, a cutting-edge large language model from Google AI, Nutrify provides personalized guidance 
+  and information to help you navigate the world of nutrition.
+  """)
+
+st.page_link("homex.py", label="Home", icon="🏠")
+st.page_link("pages/chat.py", label="Chat", icon="1️⃣")
+st.page_link("pages/about.py", label="about", icon="2️⃣")
+st.page_link("https://404foundxnutrifyai.blogspot.com/", label="Google", icon="🌎")
+
+
