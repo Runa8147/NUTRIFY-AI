a9f958ba17f7e7d2d66c7226aa49d246dcf786fb
ARUN JOHN  M S <157452839+Runa8147@users.noreply.github.com>
2024-04-08 18:54:58 +0530

    Update chat.py

--- pages/chat.py
+++ pages/chat.py
@@ -1,17 +1,19 @@
+import os
 import streamlit as st
 from time import sleep
+from dotenv import load_dotenv
 import google.generativeai as gen_ai
-# Load environment variables
 
+load_dotenv()
 
 # Configure Streamlit page settings
 st.set_page_config(
-    page_title="Nutrify AI chatbot powered by Gemini pro",
+    page_title="Gemini-Pro Chatbot by HJP7",
     page_icon=":brain:", 
     layout="centered",  # Page layout option
 )
 
-GOOGLE_API_KEY = "AIzaSyAZ35ZoRYAcU-bNLBTHQvEJjSnD9HP8CbY"
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 # Set up Google Gemini-Pro AI model
 gen_ai.configure(api_key=GOOGLE_API_KEY)
 model = gen_ai.GenerativeModel('gemini-1.0-pro')
@@ -45,10 +47,10 @@ if user_prompt:
   # Send prompt with user query
   gemini_response = st.session_state.chat_session.send_message(f"Based on a healthy diet, {user_prompt}")
 
-  # Alternative approach: Analyze keywords in response (not perfect)
-    if any(keyword in gemini_response.text.lower() for keyword in ["nutrition", "health", "diet", "exercise"]):
+  if any(keyword in gemini_response.text.lower() for keyword in ["nutrition", "health", "diet", "exercise"]):
         st.chat_message("assistant").markdown(gemini_response.text)
         like, unlike = st.columns(2)  # Two columns for feedback
+
         with like:
             if st.button(" Like"):
                 st.success("Thanks for the feedback!")
@@ -56,8 +58,9 @@ if user_prompt:
             if st.button(" Unlike"):
                 st.warning("I apologize if the information wasn't helpful. I am still under development and learning to focus on providing the best nutritional and health guidance.")
                 sleep(8)  # Optional: Display warning for a duration
+
         text_contents = gemini_response.text
         st.download_button('Download some text', text_contents)
                               
-    else:
+  else:
          st.chat_message("assistant").markdown("It seems your question might be outside my area of expertise. Please rephrase focusing on nutrition or health.")
